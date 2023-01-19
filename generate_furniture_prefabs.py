3a9032fa12a57e816bdd8774d61ab50a5f3f870a
Татьяна Ившина <tatiana.ivshina@playrix.com>
2023-01-19 11:45:59 +0300

    Add prefabs linking

--- generate_furniture_prefabs.py
+++ generate_furniture_prefabs.py
@@ -18,13 +18,14 @@ def get_purchase_ids():
     for child in room_skins:
         if child.tag == 'decorPurchases':
             purchase_id = child[0].attrib['id'][len(room_name) + 1:]
-            room_ids[purchase_id] = []
-            for skin in child[0]:
-                can_be_selected = skin[0].attrib['canBeSelected']
-                can_be_selected = True if can_be_selected == 'true' else False
-                skin_id = skin[0].attrib['id'][len(room_name) + 1:]
-                skin_id = 'empty' if skin_id == '' else skin_id
-                room_ids[purchase_id].append((can_be_selected, skin_id))
+            if purchase_id != '':
+                room_ids[purchase_id] = []
+                for skin in child[0]:
+                    can_be_selected = skin[0].attrib['canBeSelected']
+                    can_be_selected = True if can_be_selected == 'true' else False
+                    skin_id = skin[0].attrib['id'][len(room_name) + 1:]
+                    skin_id = 'empty' if skin_id == '' else skin_id
+                    room_ids[purchase_id].append((can_be_selected, skin_id))
     return room_ids
 
 
@@ -39,6 +40,11 @@ def get_uid():
 
 def generate_furniture_prefabs():
     os.makedirs('./room/FurniturePrefabs')
+    shutil.copy2('templates/Room_aN_RoomName_Furniture_template.xml',
+                 f'room/Room_a{areal}_{room_name}_Furniture.xml')
+    furniture_uid = str(get_uid())
+    link_object(furniture_uid, f'Room_a{areal}_{room_name}_Furniture', 'Background',  window.getroot()[0][4][4])
+    window.getroot()[0][4][0][3].attrib['path'] = furniture_uid
     for i in range(len(room_ids)):
         purchase = list(room_ids)[i]
         shutil.copy2('templates/FurniturePrefab_template.xml',
@@ -48,18 +54,41 @@ def generate_furniture_prefabs():
         root[0].attrib["name"] = f'{i}_{purchase}'
         root[0][0][0][1].attrib["entityId"] = str(get_uid())
         purchase_uid = str(get_uid())
+        purchase_obj_id = str(get_uid())
+        purchase_path = f'{furniture_uid}/{purchase_obj_id}/{purchase_uid}'
         root[0][2].attrib['entityId'] = purchase_uid
         redesign_node = False
         redesign_uid = 0
+        redesign_path = 0
         for j in range(len(room_ids[purchase])):
             skin = room_ids[purchase][j]
             skin_uid = create_empty_img(root, j)
+            skin_path = f'{furniture_uid}/{purchase_obj_id}/{skin_uid}'
             if skin[0] and not redesign_node:
                 redesign_node = True
                 redesign_uid = str(get_uid())
+                redesign_path = f'{furniture_uid}/{purchase_obj_id}/{redesign_uid}'
+            add_purchase(skin_path, purchase_path, redesign_path, purchase, skin[1], skin[0])
         if redesign_node:
             create_redesign_node(root, redesign_uid)
         tree.write(f'room/FurniturePrefabs/{room_name}_{purchase}.xml')
+        furniture_tree = ET.parse(f'room/Room_a{areal}_{room_name}_Furniture.xml')
+        furniture = furniture_tree.getroot()[0]
+        link_object(purchase_obj_id, f'{room_name}_{purchase}', f'{i}_{purchase}', furniture)
+        furniture_tree.write(f'room/Room_a{areal}_{room_name}_Furniture.xml')
+
+
+def link_object(obj_id, linked_object, node_name, root):
+    ET.SubElement(root, 'children')
+    root[-1].attrib['name'] = node_name
+    ET.SubElement(root[-1], 'link')
+    root[-1][0].attrib['objId'] = obj_id
+    ET.SubElement(root[-1][0], 'id')
+    root[-1][0][0].attrib['id'] = linked_object
+    root[-1][0][0].attrib['id..editor..ref'] = "assetType@sceneObject"
+    ET.SubElement(root[-1], 'transform')
+    ET.SubElement(root[-1], 'uid')
+    root[-1][-1].attrib['entityId'] = str(get_uid())
 
 
 def create_anchored_element(root):
@@ -119,6 +148,24 @@ def create_redesign_node(root, uid):
     redesign_node[-1].attrib['entityId'] = uid
 
 
+def add_purchase(skin_path, purchase_path, redesign_path, purchase, skin, is_redesign):
+    main_behaviour = window.getroot()[0][4][0][0]
+    ET.SubElement(main_behaviour, 'purchaseViews')
+    main_behaviour[-1].attrib['type'] = "purchaseView"
+    ET.SubElement(main_behaviour[-1], 'ptr')
+    main_behaviour[-1][0].attrib['autoCreateButton'] = "false"
+    main_behaviour[-1][0].attrib['purchaseId'] = f'{room_name}_{purchase}'
+    main_behaviour[-1][0].attrib['purchaseSkin'] = f'{room_name}_{skin}'
+    ET.SubElement(main_behaviour[-1][0], 'backgroundIcon')
+    ET.SubElement(main_behaviour[-1][0], 'ref')
+    main_behaviour[-1][0][-1].attrib['path'] = skin_path
+    ET.SubElement(main_behaviour[-1][0], 'selectedRef')
+    main_behaviour[-1][0][-1].attrib['path'] = purchase_path
+    if is_redesign:
+        ET.SubElement(main_behaviour[-1][0], 'pinRef')
+        main_behaviour[-1][0][-1].attrib['path'] = redesign_path
+
+
 def generate_choose_icons():
     os.makedirs('./room/ChoosePanelPrefabs')
     choose_panel = window.getroot()[0][4][7][2][1][0]
@@ -142,11 +189,12 @@ def create_window_prefab():
 
 
 if __name__ == '__main__':
-    # os.makedirs('./room')
-    # shutil.copy2('templates/Room_aN_RoomName_Window_template.xml', window_prefab)
-    # window = create_window_prefab()
+    os.makedirs('./room')
+    shutil.copy2('templates/Room_aN_RoomName_Window_template.xml', window_prefab)
+    window = create_window_prefab()
     room_ids = get_purchase_ids()
     generate_furniture_prefabs()
-    # generate_choose_icons()
+    generate_choose_icons()
+    window.write(window_prefab)
 
     print()
